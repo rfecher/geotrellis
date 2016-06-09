2a072f2bedf1f12c52a05fff9afe3f927ff48d44
James McClain <jmcclain@azavea.com>
2016-06-09 12:01:43 -0400

    Remove Excess Whitespace

--- spark/src/main/scala/geotrellis/spark/io/file/FileRDDReader.scala
+++ spark/src/main/scala/geotrellis/spark/io/file/FileRDDReader.scala
@@ -27,7 +27,7 @@ object FileRDDReader {
     numPartitions: Option[Int] = None
   )(implicit sc: SparkContext): RDD[(K, V)] = {
     if(queryKeyBounds.isEmpty) return sc.emptyRDD[(K, V)]
-    
+
     val ranges = if (queryKeyBounds.length > 1)
       MergeQueue(queryKeyBounds.flatMap(decomposeBounds))
     else

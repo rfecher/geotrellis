e9fb05f03213ecd96070616128323d266ecae1a7
James McClain <jmcclain@azavea.com>
2016-07-19 15:37:04 -0400

    COMPILES

--- spark/src/main/scala/geotrellis/spark/join/VectorJoin.scala
+++ spark/src/main/scala/geotrellis/spark/join/VectorJoin.scala
@@ -4,11 +4,15 @@ import geotrellis.vector._
 import geotrellis.spark._
 import geotrellis.spark.tiling._
 
+import com.vividsolutions.jts.geom.{Envelope => JtsEnvelope}
+import com.vividsolutions.jts.index.strtree.STRtree
 import org.apache.spark._
 import org.apache.spark.rdd._
 import org.apache.spark.SparkContext._
 
+import scala.collection.JavaConverters._
 import scala.reflect._
+
 
 object VectorJoin {
   /** Map each item to all the layout cells that it intersects with */
@@ -46,4 +50,48 @@ object VectorJoin {
         pred(l: Geometry, r: Geometry)
       }
   }
+
+  def apply[
+    L <% Geometry: ClassTag,
+    R <% Geometry: ClassTag
+  ](
+    left: RDD[L],
+    right: RDD[R],
+    pred: (Geometry, Geometry) => Boolean
+  ): RDD[(L, R)] = {
+
+    // For simplicity, assume that the right RDD has length ≥ that
+    // of the left one.
+    val rtreeRdd: RDD[STRtree] = right.mapPartitions({ partition =>
+      val rtree = new STRtree
+
+      partition.foreach({ r =>
+        val Extent(xmin, ymin, xmax, ymax) = r.envelope
+        val envelope = new JtsEnvelope(xmin, xmax, ymin, ymax)
+        rtree.insert(envelope, r)
+      })
+
+      Iterator(rtree)
+    }, preservesPartitioning = true)
+
+    // For each item in the left RDD, find the list of items in the
+    // right RDD that it intersects with.
+    left.flatMap({ l =>
+      val Extent(xmin, ymin, xmax, ymax) = l.envelope
+
+      // Find the list of items in the right RDD that this particular
+      // item from the left RDD intersects with.
+      rtreeRdd.flatMap({ tree =>
+        val envelope = new JtsEnvelope(xmin, xmax, ymin, ymax)
+
+        tree.query(envelope)
+          .asScala
+          .map({ r: Any => r.asInstanceOf[R] })
+          .filter({ r => pred(l, r) })
+          .map({ r => (l, r) })
+      }).collect
+    })
+
+  }
+
 }
